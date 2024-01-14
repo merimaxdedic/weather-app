1fdf6ea744b9cc358b984a5d2e90d14d60f7449c
Merima Dedic <merimaxdedic@gmail.com>
2024-01-14 14:42:51 -0600

    added url to api

--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 // the key to the openWeather API that we are going to use
 const MY_API_KEY = "ce21ea009e94e1fc7a863c0d2988825f";
+// a url to our API
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
