9052c08f2fd672d52f47d88f41c16d4692c273a5
Merima Dedic <merimaxdedic@gmail.com>
2024-01-14 14:44:02 -0600

    added variables to weather conditions

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 // the key to the openWeather API that we are going to use
 const MY_API_KEY = "ce21ea009e94e1fc7a863c0d2988825f";
 // a url to our API
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
+
+// this variables will be used to display the weather conditions on the browser
+let city, sunshine, cloud, humidity, temperature, windspeed, pressure,
+search_input, toggle_switch, dark_light_mode = "light", weather_details_div, parent_div, toggle_circle; 
